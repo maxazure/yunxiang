f678aa0702bcb25dd3db2f6a540a2ecc09f4df0f
i-ce-fish <876644846@qq.com>
2020-09-22 10:28:30 +0800

    日志

--- src/views/members/add.vue
+++ src/views/members/add.vue
@@ -1,68 +1,84 @@
 <template>
-  <div class='card-container'>
-    <el-card class='box-card'>
+  <div class="card-container">
+    <el-card class="box-card">
       <h3>新会员</h3>
       <el-form
-        ref='yForm'
-        :model='memberForm'
-        :rules='rules'
-        label-width='100px'
-
+        ref="yForm"
+        :model="memberForm"
+        :rules="rules"
+        label-width="100px"
       >
         <el-row>
 
           <el-col :span="12">
-            <el-form-item label='姓名:' prop='name'>
-              <component  is='YInput'  v-model='memberForm.name'
+            <el-form-item label="姓名:" prop="name">
+              <component
+                is="YInput"
+                v-model="memberForm.name"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='英文名:' prop='enname'>
-              <component  is='YInput'  v-model='memberForm.enname'
+            <el-form-item label="英文名:" prop="enname">
+              <component
+                is="YInput"
+                v-model="memberForm.enname"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='手机:' prop='mobile'>
-              <component  is='YInput'  v-model='memberForm.mobile'
+            <el-form-item label="手机:" prop="mobile">
+              <component
+                is="YInput"
+                v-model="memberForm.mobile"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='性别:' prop='gender'>
-              <component  is='YRadio'  v-model='memberForm.gender'
-                          :options="genderOptions"  />
-            </el-form-item>
-          </el-col>
-
-          <el-col :span="12">
-            <el-form-item label='生日:' prop='birthday'>
-              <component  is='YDatepicker'  v-model='memberForm.birthday'
+            <el-form-item label="性别:" prop="gender">
+              <component
+                is="YRadio"
+                v-model="memberForm.gender"
+                :options="genderOptions"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='会员卡号:' prop='vipid'>
-              <component  is='YInput'  v-model='memberForm.vipid'
+            <el-form-item label="生日:" prop="birthday">
+              <component
+                is="YDatepicker"
+                v-model="memberForm.birthday"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='微信号:' prop='wechatid'>
-              <component  is='YInput'  v-model='memberForm.wechatid'
+            <el-form-item label="会员卡号:" prop="vipid">
+              <component
+                is="YInput"
+                v-model="memberForm.vipid"
               />
             </el-form-item>
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label='地址:' prop='address'>
-              <component  is='YInput'  v-model='memberForm.address'
+            <el-form-item label="微信号:" prop="wechatid">
+              <component
+                is="YInput"
+                v-model="memberForm.wechatid"
+              />
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12">
+            <el-form-item label="地址:" prop="address">
+              <component
+                is="YInput"
+                v-model="memberForm.address"
               />
             </el-form-item>
           </el-col>
@@ -70,7 +86,7 @@
           <el-col :span="24">
             <el-form-item>
               <el-button @click="submit('memberForm')">提交</el-button>
-              <el-button @click='back'>返回</el-button>
+              <el-button @click="back">返回</el-button>
             </el-form-item>
           </el-col>
         </el-row>
@@ -80,124 +96,83 @@
 </template>
 
 <script>
-  import { addMember } from '@/api/member';
-  import request from '../../utils/request'
+import { addMember } from '@/api/member'
+import request from '../../utils/request'
 
-  export default {
-    components: {
-    },
-    data() {
-      return {
-        memberForm: {  },
-        //  apiList
+export default {
+  components: {},
+  data() {
+    return {
+      memberForm: {},
+      //  apiList
 
+      genderOptions: [],
 
-
-
-
-        genderOptions:[],
-
-
-
-
-
-
-        rules: {name:[
-          ],
-          enname:[
-          ],
-          mobile:[
-            {required:true,
-              message:'请输入手机',
-              trigger:'blur'},
-          ],
-          gender:[
-          ],
-          birthday:[
-          ],
-          vipid:[
-          ],
-          wechatid:[
-          ],
-          address:[
-          ],
-        }
-      };
-    },
-    created() {
-      //    getApiList
-
-
-
-
-
-      this.getgenderList()
-
-
-
-
-
-
-    },
-    mounted() {},
-    methods: {
-      async api() {
-        this.$router.push({ path: '/members' });
-        const res = await addMember(this.memberForm);
-      },
-      async submit(memberForm) {
-        this.$refs.yForm.validate(valid => {
-          if (valid) {
-            this.api();
-            this.$message({
-              message: '添加成功',
-              type: 'success'
-            });
-          } else {
-            return false;
+      rules: {
+        name: [],
+        enname: [],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机',
+            trigger: 'blur'
           }
-        });
-      },
-
-//    getApiList
-
-
-
-
-
-
-
-
-      async getgenderList(){
-        const response = await request({url:'/api/siteconfig/roles',method:'get'})
-        this.genderOptions = response.data
-      },
-
-
-
-
-
-
-
-
-
-
-
-
-
-      back() {
-        this.$router.push({ path: '/members' });
+        ],
+        gender: [],
+        birthday: [],
+        vipid: [],
+        wechatid: [],
+        address: []
       }
     }
-  };
-</script>
-<style lang='scss' scope>
-  .card-container {
-    background-color: #f0f2f5;
-    padding: 30px;
-    min-height: 100vh;
-    .box-card {
+  },
+  created() {
+    //    getApiList
 
+    this.getgenderList()
+  },
+  mounted() {
+  },
+  methods: {
+    async api() {
+      this.$router.push({ path: '/members' })
+      const res = await addMember(this.memberForm)
+    },
+    async submit(memberForm) {
+      this.$refs.yForm.validate(valid => {
+        if (valid) {
+          this.api()
+          this.$message({
+            message: '添加成功',
+            type: 'success'
+          })
+        } else {
+          return false
+        }
+      })
+    },
+
+    //    getApiList
+
+    async getgenderList() {
+      const response = await request({ url: '/api/siteconfig/roles', method: 'get' })
+      this.genderOptions = response.data
+    },
+
+    back() {
+      this.$router.push({ path: '/members' })
     }
   }
+}
+</script>
+<style lang='scss' scope>
+.card-container {
+  background-color: #f0f2f5;
+  padding: 30px;
+  min-height: 100vh;
+
+  .box-card {
+
+  }
+}
 </style>
